b18f10f1c53b388642866e3dd45317b615600182
TR Akshay <akshaytr19.8@outlook.com>
2023-07-28 11:23:31 +0530

    Added Spring Boot App

---
+++ src/main/java/com/example/demo/SpringDockerWithDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerWithDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerWithDbApplication.class, args);
+	}
+
+}
